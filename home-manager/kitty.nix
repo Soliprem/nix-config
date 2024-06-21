35fd3e30f244425374a12380b11cbff1d0346479
Soliprem <franci.solidoro@gmail.com>
2024-06-21 14:51:02 +0200

    update

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -10,15 +10,9 @@
       confirm_os_window_close = "0";
       remember_window_size = "no";
       disable_ligatures = "never";
-      shell = "${pkgs.tmux}/bin/tmux";
+      shell = "${pkgs.fish}/bin/fish";
       url_style = "curly";
       cursor_shape = "Underline";
-      cursor_underline_thickness = config.var.theme.border-size;
-      window_padding_width = config.var.theme.gaps-in;
-
-      # Font
-      font_family = config.var.theme.font-mono;
-      font_size = config.var.theme.font-size;
 
     };
   };
