bce1d5cfebc4941a1641b05bf7948e64c883a07b
Soliprem <franci.solidoro@gmail.com>
2024-06-21 02:30:15 +0200

    things

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,8 +40,8 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "soliprem";
+    homeDirectory = "/home/soliprem";
   };
 
   # Add stuff for your user as you see fit:
