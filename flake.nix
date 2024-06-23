c879209b54534d509ab0b4cf7e2263cbf29433e4
soliprem <franci.solidoro@gmail.com>
2024-06-23 12:37:21 +0200

    restructuring to handle multiple machines

--- flake.nix
+++ flake.nix
@@ -48,6 +48,11 @@
         # > Our main nixos configuration file <
         modules = [./nixos/configuration.nix];
       };
+      nixos-pc = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/pc/configuration.nix];
+      };
     };
 
     # Standalone home-manager configuration entrypoint
@@ -55,6 +60,17 @@
     homeConfigurations = {
       # FIXME replace with your username@hostname
       "soliprem@nixos-laptop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [
+          ./home-manager/home.nix
+          nvf.homeManagerModules.default
+          stylix.homeManagerModules.stylix
+          hyprland.homeManagerModules.default
+        ];
+      };
+      "soliprem@nixos-pc" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
