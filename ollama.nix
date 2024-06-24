bd24b6d0d6d2ccad0fa378f5f077a4d6c724d2fa
Soliprem <franci.solidoro@gmail.com>
2024-06-24 14:10:17 +0200

    ollama GPU acceleration

--- ollama.nix
+++ ollama.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-rocm;
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1030";
+      HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+    };
   };
 }
