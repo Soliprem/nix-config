18a5023cd2792d248c1922686a69e1bdcb79191b
Soliprem <franci.solidoro@gmail.com>
2024-10-29 21:11:59 +0100

    hyprland: animations and binds

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -263,7 +263,7 @@ in {
           "$mod, b, exec, notify-battery"
           ", Print, exec, grimblast copy area"
           "$mod, E, exec, nautilus --new-window"
-          "$mod+Shift, Period, exec, fuzzel-emoji"
+          "$mod+Shift+Alt, Period, exec, fuzzel-emoji"
           "$mod, Comma, focusmonitor, l"
           "$mod, Period, focusmonitor, r"
           "$mod+Shift, Comma, movewindow, mon:l"
