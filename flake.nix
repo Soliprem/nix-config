f64a26ead958685b5e06464f00086c812095c36e
Soliprem <franci.solidoro@gmail.com>
2024-10-29 15:59:18 +0100

    hyprland: turning towards libastal

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    soluastal.url = "github:soliprem/soluastal";
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -2,10 +2,44 @@
 {
   inputs,
   pkgs,
+  lib,
   config,
   ...
 }: let
   inherit (config.lib.stylix) colors;
+  gamemode = pkgs.writeShellApplication {
+    name = "gamemode";
+    text = ''
+      HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+      if [ "$HYPRGAMEMODE" = 1 ] ; then
+          hyprctl --batch "\
+              keyword animations:enabled 0;\
+              keyword decoration:drop_shadow 0;\
+              keyword decoration:blur:enabled 0;\
+              keyword general:gaps_in 0;\
+              keyword general:gaps_out 0;\
+              keyword general:border_size 1;\
+              keyword decoration:rounding 0"
+          exit
+      fi
+      hyprctl reload
+    '';
+  };
+  togglebar = pkgs.writeShellApplication {
+    name = "togglebar";
+    runtimeInputs = [inputs.soluastal.packages.${pkgs.system}.default];
+    text = ''
+      LOCKFILE="/tmp/soluastal"
+      if [[ $(cat $LOCKFILE) ]]; then
+          kill "$(cat $LOCKFILE)"
+          rm $LOCKFILE
+      else
+          touch "$LOCKFILE"
+          astal-lua &
+          echo $! >> $LOCKFILE
+      fi
+    '';
+  };
 in {
   wayland.windowManager.hyprland = {
     enable = true;
@@ -72,7 +106,7 @@ in {
           "overshot, 0.05, 0.9, 0.1, 1.1"
           "crazyshot, 0.1, 1.5, 0.76, 0.92 "
           "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
-          "fluent_decel, 0.1, 1, 0, 1"
+          "fluent_decel, 0, 1, 0, 1"
           "easeInOutCirc, 0.85, 0, 0.15, 1"
           "easeOutCirc, 0, 0.55, 0.45, 1"
           "easeOutExpo, 0.16, 1, 0.3, 1"
@@ -82,8 +116,8 @@ in {
           "windows, 1, 3, md3_decel, popin 60%"
           "border, 1, 10, default"
           "fade, 1, 2.5, md3_decel"
-          "workspaces, 1, 7, fluent_decel, slide"
-          "specialWorkspace, 1, 3, md3_decel, slidevert"
+          "workspaces, 1, 7, fluent_decel, slidevert"
+          "specialWorkspace, 1, 3, md3_decel, slide"
         ];
       };
 
@@ -174,7 +208,7 @@ in {
         # Gaps and border
         gaps_in = 4;
         gaps_out = 5;
-        gaps_workspaces = 50;
+        # gaps_workspaces = 50;
         border_size = 1;
 
         resize_on_border = true;
@@ -220,6 +254,8 @@ in {
           "$mod, V, exec, pkill fuzzel || cliphist list | fuzzel --dmenu | cliphist decode | wl-copy"
           # "$mod, V, exec, pkill walker || cliphist list | walker -dk | cliphist decode | wl-copy"
           "$mod,tab,focuscurrentorlast"
+          "$mod, F1, exec, ${lib.getExe gamemode}"
+          "$mod, F2, exec, ${lib.getExe togglebar}"
           "$mod+Shift, space, togglefloating"
           "$mod+Alt, l, exec, hyprlock --immediate"
           "$mod, t, exec, notify-time"
