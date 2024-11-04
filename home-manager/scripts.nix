f60864f17f57b639fa4b33eaa16ee0c284b4ee5e
Soliprem <franci.solidoro@gmail.com>
2024-11-04 19:49:49 +0100

    scripts: init

---
+++ home-manager/scripts.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: let
+    wayshotpick = pkgs.writeShellApplication {
+    name = "gamemode";
+    text = ''
+      case "$(printf "a selected area\\nfull screen\\na selected area (copy)\\nfull screen (copy)" |\
+        fuzzel --dmenu -l 6 -i -p "Screenshot which area?")" in
+          "a selected area") wayshot -s "$(slurp -f '%x %y %w %h')" -f\
+            ~/Pictures/wayshot/sel-area-"$(date '+%y%m%d-%H%M-%S').png" ;;
+          "full screen") wayshot -f ~/Pictures/wayshot/pic-full-"$(date '+%y%m%d-%H%M-%S').png" ;;
+          "a selected area (copy)") wayshot -s "$(slurp -f '%x %y %w %h')" --stdout | wl-copy ;;
+          "full screen (copy)") wayshot --stdout | wl-copy ;;
+      esac
+    '';
+  };
+
+in {
+  home.homePackages = [ wayshotpick ];
+}
