8e02bfc0061243e28e530222fcd6765bec97713c
Soliprem <franci.solidoro@gmail.com>
2024-11-17 08:54:30 +0100

    qute: added dandwiki

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -3,6 +3,10 @@ _: {
     enable = true;
     settings = {
       tabs.show = "switching";
+      colors.webpage.darkmode.enabled = false;
     };
+      searchEngines = {
+        d = "https://www.dandwiki.com/w/index.php?title=Special%3ASearch&search={}&go=Go";
+      };
   };
 }
