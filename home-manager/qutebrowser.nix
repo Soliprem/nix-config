c1be1637bf6ac385127df61b5434b28a6a1642ed
Soliprem <franci.solidoro@gmail.com>
2024-11-18 07:02:15 +0100

    qutebrowser: added homepage

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -4,6 +4,10 @@ _: {
     settings = {
       tabs.show = "switching";
       colors.webpage.darkmode.enabled = false;
+      url = {
+        start_pages = "~/.config/homepage/homepage.html";
+        default_page = "~/.config/homepage/homepage.html";
+      };
     };
     searchEngines = {
       d = "https://www.dandwiki.com/w/index.php?title=Special%3ASearch&search={}&go=Go";
