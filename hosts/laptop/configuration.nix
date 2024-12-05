f42d40049d5e330c769062fe1e0e5fe3b4423b8a
Soliprem <franci.solidoro@gmail.com>
2024-12-05 11:01:53 +0100

    moving to hosts

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,175 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-laptop"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  #systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText"openssl.cnf""
+  #openssl_conf = openssl_init
+  #[openssl_init]
+  #ssl_conf = ssl_sect
+  #[ssl_sect]
+  #Options = UnsalfeLegacyRenegotation
+  #[system_default_sect]
+  #CipherString = Default:@SECLEVEL=0
+  #"
+  #;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  # services = {
+  #   # xserver.enable = true;
+  #
+  #   # Enable the KDE Plasma Desktop Environment.
+  #   # displayManager.sddm.enable = true;
+  #   # services.desktopManager.plasma6.enable = true;
+  #
+  #   # Configure keymap in X11
+  #   xserver.xkb = {
+  #     layout = "eu";
+  #     options = "caps:swapescape";
+  #   };
+  #   printing.enable = true;
+  # };
+
+  # Enable CUPS to print documents.
+
+  # Enable sound with pipewire.
+  hardware = {
+    pulseaudio.enable = false;
+    sensor.iio.enable = true;
+    bluetooth.enable = true;
+  };
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+          };
+        };
+      };
+    };
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # install fish
+  programs.fish.enable = true;
+  programs.zsh.enable = true;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.soliprem = {
+    isNormalUser = true;
+    description = "Francesco Prem Solidoro";
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      # thunderbird
+      # mommy
+      # starship
+      # lolcat
+      # cowsay
+      # fortune
+      # armcord
+      wl-clipboard
+    ];
+    shell = pkgs.nushell;
+  };
+
+  # Install firefox.
+  # programs.firefox.enable = true;
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   # neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   git
+  #   wget
+  #   fastfetch
+  #   libgccjit
+  #   linearicons-free
+  #   libnotify
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+}
