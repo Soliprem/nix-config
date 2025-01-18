0b4e1a7a14fffd78417a72823a2ada0a6ec57a73
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-01-18 18:49:30 +0100

    home-manager: rm

--- home-manager/hypr/packages.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, inputs, ...}: {
-  home.packages = with pkgs; [
-    gammastep
-    inputs.iio-hyprland.packages.${pkgs.system}.default
-    inputs.astal.packages.${pkgs.system}.io
-    inputs.sash.packages.${pkgs.system}.default
-  ];
-}
--- home-manager/swayosd.nix
+++
@@ -1,5 +0,0 @@
-_:{
-  services.swayosd = {
-    enable = true;
-  };
-}
