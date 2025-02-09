69fba0f09d0cfde42565cb7cb07860e277a4443a
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-02-09 13:26:02 +0100

    mime: new defaults

--- system/mime.nix
+++ system/mime.nix
@@ -7,6 +7,20 @@ _: {
       "x-scheme-handler/https" = ["zen.desktop" "org.qutebrowser.qutebrowser.desktop"];
       "x-scheme-handler/about" = ["zen.desktop" "org.qutebrowser.qutebrowser.desktop"];
       "x-scheme-handler/unknown" = ["zen.desktop" "org.qutebrowser.qutebrowser.desktop"];
+      "x-scheme-handler/nxm" = "vortex-downloads-handler.desktop";
+      "x-scheme-handler/nxm-protocol" = "vortex-downloads-handler.desktop";
+      "video/mp2t" = "mpv.desktop";
+      "video/vnd.avi" = "mpv.desktop";
+      "video/quicktime" = "mpv.desktop";
+      "video/x-flv" = "mpv.desktop";
+      "video/vnd.mpegurl" = "mpv.desktop";
+      "video/mpeg" = "mpv.desktop";
+      "video/dv" = "mpv.desktop";
+      "video/x-flic" = "mpv.desktop";
+      "video/vnd.rn-realvideo" = "mpv.desktop";
+      "video/webm" = "mpv.desktop";
+      "video/mp4" = "mpv.desktop";
+      "video/3gpp2" = "mpv.desktop";
     };
   };
 }
