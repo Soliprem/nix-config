93fc0011fa9839968894050a4e6b78bb8b0faf9c
Soliprem <franci.solidoro@gmail.com>
2025-06-27 23:35:16 +0200

    laptop: update config to add plymouth

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -13,8 +13,13 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth.enable = true;
+  };
 
   networking.hostName = "nixos-laptop"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -55,20 +60,6 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  # services = {
-  #   # xserver.enable = true;
-  #
-  #   # Enable the KDE Plasma Desktop Environment.
-  #   # displayManager.sddm.enable = true;
-  #   # services.desktopManager.plasma6.enable = true;
-  #
-  #   # Configure keymap in X11
-  #   xserver.xkb = {
-  #     layout = "eu";
-  #     options = "caps:swapescape";
-  #   };
-  #   printing.enable = true;
-  # };
 
   # Enable CUPS to print documents.
 
