55cd6eb22c866569758d93d9ed9b83613c006306
Soliprem <franci.solidoro@gmail.com>
2025-07-04 02:29:17 +0200

    nvf: avante configured

--- system/modules/nvf.nix
+++ system/modules/nvf.nix
@@ -318,12 +318,43 @@ in {
             };
           };
         };
-
         assistant = {
-          avante-nvim.enable = true;
+          avante-nvim = {
+            enable = true;
+            setupOpts = {
+              provider = "ollama";
+              providers = {
+                openai = {
+                  endpoint = "https://api.openai.com/v1";
+                  model = "gpt-4o"; # your desired model (or use gpt-4o, etc.)
+                  timeout = 30000; # Timeout in milliseconds, increase this for reasoning models
+                  extra_request_body = {
+                    temperature = 0;
+                    max_completion_tokens = 8192; # Increase this to include reasoning tokens (for reasoning models)
+                    reasoning_effort = "medium"; # low|medium|high, only used for reasoning models
+                  };
+                };
+                groq = {
+                  __inherited_from = "openai";
+                  api_key_name = "groq";
+                  endpoint = "https://api.groq.com/openai/v1/";
+                  model = "llama-3.3-70b-versatile";
+                  # disable_tools = true;
+                  # extra_request_body = {
+                  #   temperature = 1;
+                  #   max_tokens = 32768; # remember to increase this value, otherwise it will stop generating halfway
+                  # };
+                };
+                ollama = {
+                  endpoint = "http://127.0.0.1:11434";
+                  model = "mistral:latest";
+                };
+              };
+            };
+          };
           chatgpt.enable = false;
           codecompanion-nvim = {
-            enable = true;
+            enable = false;
             setupOpts = {
               strategies = {
                 chat = {
