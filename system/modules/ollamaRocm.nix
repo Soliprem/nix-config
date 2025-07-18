0703fe670911aeac6bc10f199078e137325f3eea
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-07-19 01:26:37 +0200

    ollamaRocm: don't need overrides

--- system/modules/ollamaRocm.nix
+++ system/modules/ollamaRocm.nix
@@ -2,10 +2,10 @@ _: {
   services.ollama = {
     enable = true;
     acceleration = "rocm";
-    environmentVariables = {
-      HCC_AMDGPU_TARGET = "gfx1030";
-      "TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL" = "1";
-    };
-    rocmOverrideGfx = "10.3.0";
+    # environmentVariables = {
+    #   HCC_AMDGPU_TARGET = "gfx1201";
+    #   "TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL" = "1";
+    # };
+    # rocmOverrideGfx = "12.0.1";
   };
 }
