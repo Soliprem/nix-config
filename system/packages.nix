d6ff76bf8e61d71c214b424ce795a03fcee06a80
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-09-08 10:11:13 +0200

    flake: update lock

--- system/packages.nix
+++ system/packages.nix
@@ -100,17 +100,17 @@
     nix-output-monitor
     nexusmods-app-unfree
     mangohud
-    # inputs.ghostty.packages.${system}.default
-    (inputs.ghostty.packages.${system}.default.overrideAttrs (drv: {
-      patches =
-        drv.patches or []
-        ++ [
-          (pkgs.fetchpatch {
-            url = "https://github.com/Opposite34/ghostty/commit/5b871c595254eece6bf44ab48f71409b7ed36088.patch";
-            hash = "sha256-hCWp2MdoD89oYN3I+Pq/HW4k4RcozS1tDuXHO3Nd+Y8=";
-          })
-        ];
-    }))
+    inputs.ghostty.packages.${system}.default
+    # (inputs.ghostty.packages.${system}.default.overrideAttrs (drv: {
+    #   patches =
+    #     drv.patches or []
+    #     ++ [
+    #       (pkgs.fetchpatch {
+    #         url = "https://github.com/Opposite34/ghostty/commit/5b871c595254eece6bf44ab48f71409b7ed36088.patch";
+    #         hash = "sha256-hCWp2MdoD89oYN3I+Pq/HW4k4RcozS1tDuXHO3Nd+Y8=";
+    #       })
+    #     ];
+    # }))
     foot
     # fluffychat
     bitwarden-desktop
