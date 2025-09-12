682cf07fa09414b5e622f20392752daf512ad423
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-09-12 21:33:23 +0200

    packages: add limo, remove old ghostty patch

--- system/packages.nix
+++ system/packages.nix
@@ -100,18 +100,9 @@
     ddcui
     nix-output-monitor
     nexusmods-app-unfree
+    limo
     mangohud
     inputs.ghostty.packages.${system}.default
-    # (inputs.ghostty.packages.${system}.default.overrideAttrs (drv: {
-    #   patches =
-    #     drv.patches or []
-    #     ++ [
-    #       (pkgs.fetchpatch {
-    #         url = "https://github.com/Opposite34/ghostty/commit/5b871c595254eece6bf44ab48f71409b7ed36088.patch";
-    #         hash = "sha256-hCWp2MdoD89oYN3I+Pq/HW4k4RcozS1tDuXHO3Nd+Y8=";
-    #       })
-    #     ];
-    # }))
     foot
     # fluffychat
     bitwarden-desktop
