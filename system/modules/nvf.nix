a9f64b4ff13a2c7d3d8cb0c0b56d4c7cd615524d
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-10-23 16:15:46 +0200

    nvf: switch model for avante

--- system/modules/nvf.nix
+++ system/modules/nvf.nix
@@ -348,7 +348,7 @@
                 };
                 ollama = {
                   endpoint = "http://127.0.0.1:11434";
-                  model = "qwen2.5-coder:14b";
+                  model = "gpt-oss:latest";
                 };
               };
             };
