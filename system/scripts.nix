ac4ee77273b136ad5ce0c5219f0a5456f4e702ec
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-10-24 19:43:25 +0200

    scripts: use wmenu instead of fuzzel

--- system/scripts.nix
+++ system/scripts.nix
@@ -91,7 +91,7 @@
       runtimeInputs = with pkgs; [wayshot slurp];
       text = ''
         case "$(printf "a selected area\\nfull screen\\na selected area (copy)\\nfull screen (copy)" |\
-          fuzzel --dmenu -l 6 -i -p "Screenshot which area?")" in
+          wmenu -ip "Screenshot which area?")" in
             "a selected area") wayshot -s "$(slurp -f '%x %y %w %h')" -f\
               ~/Pictures/wayshot/sel-area-"$(date '+%y%m%d-%H%M-%S').png" ;;
             "full screen") wayshot -f ~/Pictures/wayshot/pic-full-"$(date '+%y%m%d-%H%M-%S').png" ;;
@@ -108,7 +108,7 @@
                   mode="$1"
                 else
                   mode="$(printf "region\\n\\nall" |\
-                        fuzzel --dmenu -l 6 -i -p "Screenshot which area?")"
+                        wmenu -ip "Screenshot which area?")"
                 fi
 
                 case $mode in
