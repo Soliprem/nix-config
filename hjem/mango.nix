a17bc1206ab71fb322da63f017d4a5b66e2a0d7c
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-10-28 00:22:46 +0100

    mango: border_when_single

--- hjem/mango.nix
+++ hjem/mango.nix
@@ -73,7 +73,7 @@
       overviewgappo=30
 
       # Misc
-      no_border_when_single=1
+      no_border_when_single=0
       axis_bind_apply_timeout=100
       focus_on_activate=1
       inhibit_regardless_of_visibility=0
