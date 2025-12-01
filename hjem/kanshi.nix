425ac0c60cb62a74a1b2f2e6300defdc79d3e166
Soliprem <franci.solidoro@gmail.com>
2025-12-01 12:18:07 +0100

    kanshi: fix laptop only profile

--- hjem/kanshi.nix
+++ hjem/kanshi.nix
@@ -10,13 +10,17 @@ _: {
 
       # Profile 2: Projector Presentation
       profile {
-          output "eDP-1" enable mode 1920x1200@60.003 position 0,0
+          output "eDP-1" enable mode 1920x1200@60 position 0,0 scale 1.5
           output "Seiko Epson Corporation EPSON PJ 0x01010101" scale 1.5 position 1920,0
       }
 
       # Profile 3: Laptop Only (Fallback)
       profile {
-          output "eDP-1" enable mode 1920x1200@60.003
+          output "eDP-1" enable mode 1920x1200@60 scale 1.2
+      }
+
+      profile {
+          output "eDP-1" enable mode 1920x1200@60 scale 1.2
           output "*" enable scale 1
       }
     '';
