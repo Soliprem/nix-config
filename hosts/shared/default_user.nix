46b13fccc647ddf6041312b62e3b5615b67f68cb
Soliprem <franci.solidoro@gmail.com>
2025-12-03 19:26:04 +0100

    default_user: use bash as login shell

--- hosts/shared/default_user.nix
+++ hosts/shared/default_user.nix
@@ -3,6 +3,16 @@
     isNormalUser = true;
     description = "Francesco Prem Solidoro";
     extraGroups = ["networkmanager" "wheel" "docker"];
-    shell = pkgs.nushell;
+    shell = pkgs.bash;
   };
+
+  environment.shells = [
+    pkgs.nushell
+  ];
+
+  programs.bash.interactiveShellInit = ''
+    if ! [ "$TERM" = "dumb" ] && [ -z "$BASH_EXECUTION_STRING" ]; then
+      exec nu
+    fi
+  '';
 }
