4ce074a2c961e1951db44cfed08ff166079ef77b
Soliprem <franci.solidoro@gmail.com>
2025-12-03 18:20:59 +0100

    darkman: better separation of stuffs

--- hjem/darkman.nix
+++ hjem/darkman.nix
@@ -1,17 +1,33 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
+let
+  switch-dark = pkgs.writeShellApplication {
+    name = "switch-dark";
+    runtimeInputs = with pkgs; [ matugen glib gsettings-desktop-schemas swww ];
+    text = ''
+      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS
+      matugen image ~/.config/bg -m dark
+    '';
+  };
+
+  switch-light = pkgs.writeShellApplication {
+    name = "switch-light";
+    runtimeInputs = with pkgs; [ matugen glib gsettings-desktop-schemas swww ];
+    text = ''
+      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS
+      matugen image ~/.config/bg -m light
+    '';
+  };
+in
 {
   hjem.users.soliprem.files = {
-    ".config/darkman/config.yml".text = lib.generators.toYAML {
+    ".config/darkman/config.yml".source = pkgs.writers.writeYAML "config.yml" {
       lat = 41.9;
       lng = 12.5;
       usegeoclue = true;
       dbusserver = true;
     };
-    ".local/share/dark-mode.d/nixrice".source = pkgs.writers.writeBash /* bash */ ''
-      nixrice ~/.config/bg dark
-    '';
-    ".local/share/light-mode.d/nixrice".source = pkgs.writers.writeBash /* bash */ ''
-      nixrice ~/.config/bg light
-    '';
+
+    ".local/share/dark-mode.d/switch-dark".source = "${switch-dark}/bin/switch-dark";
+    ".local/share/light-mode.d/switch-light".source = "${switch-light}/bin/switch-light";
   };
 }
