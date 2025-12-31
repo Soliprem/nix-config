ac5f4d1f9a982d5cc01415b42ff89d2a825abd53
Soliprem <franci.solidoro@gmail.com>
2025-12-31 21:11:44 +0100

    nushell: add nd -> nix develop alias

--- hjem/nushell.nix
+++ hjem/nushell.nix
@@ -35,6 +35,7 @@ _: {
 
 
       alias D = cd $'($env.HOME)Downloads'; ls -a
+      alias nd = nix develop
       alias sl = sll
       alias fg = job unfreeze
       alias ,, = , (history | last | get command)
