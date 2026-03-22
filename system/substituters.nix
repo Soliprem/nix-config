d8faec395d08ff9cd7c15d1f9ae502792b4baafa
Soliprem <franci.solidoro@gmail.com>
2026-03-22 01:05:21 +0100

    substituters: remove unused

--- system/substituters.nix
+++ system/substituters.nix
@@ -7,13 +7,9 @@
       ];
       substituters = [
         "https://hyprland.cachix.org"
-        "https://cosmic.cachix.org/"
-        "https://tola.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "tola.cachix.org-1:5hMwVpNfWcOlq0MyYuU9QOoNr6bRcRzXBMt/Ua2NbgA="
       ];
     };
     optimise.automatic = true;
