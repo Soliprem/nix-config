24b4c3d1ae3a61eb0c0ae6fbee4197fb90b81c54
Soliprem <franci.solidoro@gmail.com>
2026-04-07 01:49:37 +0200

    ghostty: use fish

--- hjem/ghostty.nix
+++ hjem/ghostty.nix
@@ -2,7 +2,7 @@
 {
   files = {
     ".config/ghostty/config".text = ''
-      command = ${pkgs.nushell}/bin/nu
+      command = ${pkgs.fish}/bin/fish
       background-opacity = 0.78
       gtk-single-instance = true
       gtk-titlebar = false
