0ac77a295ec448f28dde94d9237ce6a0b8138483
Soliprem <franci.solidoro@gmail.com>
2026-04-11 04:18:10 +0200

    ollama laptop: update

--- system/modules/ollama.nix
+++ system/modules/ollama.nix
@@ -1,5 +1,13 @@
-_: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  sys = pkgs.stdenv.hostPlatform.system;
+  unstable-pkgs = inputs.nixpkgs-unstable.legacyPackages.${sys};
+in {
   services.ollama = {
     enable = true;
+    package = unstable-pkgs.ollama;
   };
 }
