85548be38aad23711fcad7831b68984a0bae4096
Soliprem <franci.solidoro@gmail.com>
2026-04-14 17:58:42 +0200

    ollama: fix envvars

--- system/modules/ollamaRocm.nix
+++ system/modules/ollamaRocm.nix
@@ -1,16 +1,18 @@
-{ pkgs, inputs, ... }:
-let
+{
+  pkgs,
+  inputs,
+  ...
+}: let
   sys = pkgs.stdenv.hostPlatform.system;
   unstable-pkgs = inputs.nixpkgs-unstable.legacyPackages.${sys};
-in 
-{
+in {
   services.ollama = {
     enable = true;
     package = unstable-pkgs.ollama-rocm;
-    # environmentVariables = {
-    #   HCC_AMDGPU_TARGET = "gfx1201";
-    #   "TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL" = "1";
-    # };
+    environmentVariables = {
+      HIP_VISIBLE_DEVICES = "0";
+      ROCR_VISIBLE_DEVICES = "0";
+    };
     # rocmOverrideGfx = "12.0.1";
   };
 }
