08248b94579732885ef5055c83a8d0d7950de1a7
Soliprem <franci.solidoro@gmail.com>
2026-05-04 14:57:59 +0200

    keyd: fix

--- system/modules/keyd.nix
+++ system/modules/keyd.nix
@@ -7,10 +7,10 @@ _:{
         settings = {
           global.overload_tap_timeout = 200;
           main = {
+            esc = "capslock";
+            capslock = "overload(control,esc)";
             leftshift = "overload(shift, S-9)";
             rightshift = "overload(shift, S-0)";
-            capslock = "overload(control,esc)";
-            "control+esc" = "capslock";
           };
         };
       };
