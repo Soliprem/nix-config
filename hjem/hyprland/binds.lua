92d3010efe1c8f248e206a4e2f2f889d85118155
Soliprem <franci.solidoro@gmail.com>
2026-05-12 21:13:58 +0200

    hjem/hyprland: restructure binds, add layout specific binds, add layout switching

--- hjem/hyprland/binds.lua
+++ hjem/hyprland/binds.lua
@@ -4,6 +4,7 @@ local mod = H.mod
 local term = H.term
 local browser = H.browser
 
+
 local keybinds = {
   CTRL = {
     ALT = {
@@ -18,27 +19,19 @@ local keybinds = {
   SUPER = {
     SHIFT = {
       E = { action = hl.dsp.exit(), },
-      h = { action = hl.dsp.layout("consume_or_expel prev"), },
-      l = { action = hl.dsp.layout("consume_or_expel next"), },
-      r = { action = hl.dsp.layout("colresize -conf"), },
-      equal = { action = hl.dsp.layout("colresize +0.02"), },
       Comma = { action = hl.dsp.window.move({ monitor = "-1" }), },
       Period = { action = hl.dsp.window.move({ monitor = "+1" }), },
       space = { action = hl.dsp.window.float({ action = "toggle" }), },
       f = { action = hl.dsp.window.fullscreen({ mode = "fullscreen" }), },
     },
     CTRL = {
-      h = { action = hl.dsp.layout("swapcol l"), },
-      l = { action = hl.dsp.layout("swapcol r"), },
-      space = { action = hl.dsp.layout("swapwithmaster"), },
       Comma = { action = hl.dsp.workspace.move({ monitor = "-1" }), },
       Period = { action = hl.dsp.workspace.move({ monitor = "+1" }), },
-      SHIFT = {
-        h = { action = hl.dsp.layout("addmaster"), },
-        l = { action = hl.dsp.layout("removemaster"), },
-      },
     },
     ALT = {
+      T = { action = function() H.set_layout("master") end },
+      M = { action = function() H.set_layout("monocle") end },
+      X = { action = function() H.set_layout("scrolling") end },
       s = { action = hl.dsp.window.move({ workspace = "special" }), },
       p = { action = hl.dsp.window.pin(), },
       F = { action = hl.dsp.window.fullscreen_state({ internal = -1, client = 2 }), },
@@ -48,13 +41,6 @@ local keybinds = {
     z = { action = hl.dsp.window.drag(), opts = { mouse = true }, },
     ["mouse:273"] = { action = hl.dsp.window.resize(), opts = { mouse = true }, },
     Tab = { action = hl.dsp.focus({ last = true }), },
-    j = { action = hl.dsp.layout("focus d"), },
-    k = { action = hl.dsp.layout("focus u"), },
-    h = { action = hl.dsp.layout("focus l"), },
-    l = { action = hl.dsp.layout("focus r"), },
-    r = { action = hl.dsp.layout("colresize +conf"), },
-    equal = { action = hl.dsp.layout("colresize -0.02"), },
-    space = { action = hl.dsp.layout("promote"), },
     Comma = { action = hl.dsp.focus({ monitor = "-1" }), },
     Period = { action = hl.dsp.focus({ monitor = "+1" }), },
     s = { action = hl.dsp.workspace.toggle_special(""), },
@@ -115,34 +101,84 @@ local execs = {
   },
 }
 
-local function key_table_parser(key_table, path, method)
-  path = path or {}
-  for key, value in pairs(key_table) do
-    local new_path = { table.unpack(path) }
-    table.insert(new_path, key)
-    if value.action then
-      method(table.concat(new_path, "+"), value.action, value.opts)
-    else
-      key_table_parser(value, new_path, method)
-    end
-  end
-end
+local scrolling_binds = {
+  SUPER = {
+    SHIFT = {
+      h = { action = hl.dsp.layout("consume_or_expel prev") },
+      l = { action = hl.dsp.layout("consume_or_expel next") },
+      r = { action = hl.dsp.layout("colresize -conf") },
+      equal = { action = hl.dsp.layout("colresize +0.02") },
+    },
+    CTRL = {
+      h = { action = hl.dsp.layout("swapcol l") },
+      l = { action = hl.dsp.layout("swapcol r") },
+    },
+    h = { action = hl.dsp.layout("focus l") },
+    l = { action = hl.dsp.layout("focus r") },
+    j = { action = hl.dsp.layout("focus d") },
+    k = { action = hl.dsp.layout("focus u") },
+    r = { action = hl.dsp.layout("colresize +conf") },
+    equal = { action = hl.dsp.layout("colresize -0.02") },
+    space = { action = hl.dsp.layout("promote") },
+  },
+}
 
-key_table_parser(keybinds, {}, hl.bind)
-key_table_parser(execs, {}, H.exec)
+local master_binds = {
+  SUPER = {
+    SHIFT = {
+      j = { action = hl.dsp.layout("swapnext") },
+      k = { action = hl.dsp.layout("swapprev") },
+      equal = { action = hl.dsp.layout("mfact +0.05") },
+      h = { action = hl.dsp.layout("addmaster") },
+      l = { action = hl.dsp.layout("removemaster") },
+    },
+    j = { action = hl.dsp.layout("cyclenext") },
+    k = { action = hl.dsp.layout("cycleprev") },
+    equal = { action = hl.dsp.layout("mfact -0.05") },
+    space = { action = hl.dsp.layout("swapwithmaster") },
+  },
+}
+
+local monocle_binds = {
+  SUPER = {
+    SHIFT = {
+      j = { action = hl.dsp.window.swap({ next = true }) },
+      k = { action = hl.dsp.window.swap({ prev = true }) },
+    },
+    j = { action = hl.dsp.layout("cyclenext") },
+    k = { action = hl.dsp.layout("cycleprev") },
+  },
+}
+
+H.key_table_parser(keybinds, {}, hl.bind, { submap_universal = true })
+H.key_table_parser(execs, {}, H.exec, { submap_universal = true })
+
+hl.define_submap("layout_scrolling", function()
+  H.key_table_parser(scrolling_binds, {}, hl.bind)
+end)
+
+hl.define_submap("layout_master", function()
+  H.key_table_parser(master_binds, {}, hl.bind)
+end)
+
+hl.define_submap("layout_monocle", function()
+  H.key_table_parser(monocle_binds, {}, hl.bind)
+end)
+
+H.sync_layout_submap()
 
 for i = 1, 10 do
   local key = i % 10
 
   hl.bind(mod .. " + " .. key, function()
     H.focus_split_workspace(i)
-  end)
+  end, { submap_universal = true })
 
   hl.bind(mod .. " + SHIFT + " .. key, function()
     H.move_to_split_workspace(i, false)
-  end)
+  end, { submap_universal = true })
 
   hl.bind(mod .. " + ALT + SHIFT + " .. key, function()
     H.move_to_split_workspace(i, true)
-  end)
+  end, { submap_universal = true })
 end
--- hjem/hyprland/helpers.lua
+++ hjem/hyprland/helpers.lua
@@ -28,4 +28,35 @@ function M.move_to_split_workspace(n, follow)
   }))
 end
 
+function M.sync_layout_submap()
+  local ws = hl.get_active_workspace()
+  local layout = ws and ws.tiled_layout or "scrolling"
+  hl.dispatch(hl.dsp.submap("layout_" .. layout))
+end
+
+function M.set_layout(layout)
+  hl.config({ general = { layout = layout } })
+  hl.dispatch(hl.dsp.submap("layout_" .. layout))
+end
+
+function M.key_table_parser(key_table, path, method, default_opts)
+  path = path or {}
+  for key, value in pairs(key_table) do
+    local new_path = { table.unpack(path) }
+    table.insert(new_path, key)
+    if value.action then
+      local opts = default_opts
+      if value.opts then
+        opts = {}
+        for k, v in pairs(default_opts or {}) do opts[k] = v end
+        for k, v in pairs(value.opts) do opts[k] = v end
+      end
+      method(table.concat(new_path, "+"), value.action, opts)
+    else
+      M.key_table_parser(value, new_path, method, default_opts)
+    end
+  end
+end
+
+
 return M
