aa8c6829925bad332508204e34ffc48627c8ae4d
Soliprem <franci.solidoro@gmail.com>
2026-05-16 01:40:17 +0200

    greeter: back to ly it is
    begone, greetd (too much fuss)
    regreet straight up exploded. Never trusting GUIs again

--- system/modules/greeter.nix
+++ system/modules/greeter.nix
@@ -1,27 +1,3 @@
-{ pkgs, ... }: {
-  programs.regreet = {
-    enable = true;
-    settings = {
-      background = {
-        path = ../../assets/greeter-space-monochrome.jpg;
-        fit = "Cover";
-      };
-      GTK = {
-        application_prefer_dark_theme = true;
-      };
-    };
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    font = {
-      package = pkgs.nerd-fonts.inconsolata;
-      name = "Inconsolata Nerd Font";
-      size = 11;
-    };
-    cursorTheme = {
-      name = "Bibata-Modern-Ice";
-      package = pkgs.bibata-cursors;
-    };
-  };
+_: {
+  services.displayManager.ly.enable = true;
 }
