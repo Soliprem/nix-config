9d06f411d1802f7252d793db3841f05bc90202e9
Soliprem <franci.solidoro@gmail.com>
2026-05-20 00:35:04 +0200

    add substituters

--- system/substituters.nix
+++ system/substituters.nix
@@ -7,9 +7,13 @@
       ];
       substituters = [
         "https://hyprland.cachix.org"
+        "https://nyx.cachix.org"
+        "https://soliprem.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
+        "soliprem.cachix.org-1:kwxQ2xmtoQJVrKUBvSkG/+03ZTuxn06Aebh8AZvtMPA="
       ];
     };
     optimise.automatic = true;
