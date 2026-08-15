a77e6e55a6f7d530ee6672bf03c903f110745e30
Soliprem <franci.solidoro@gmail.com>
2026-08-15 09:41:45 +0200

    subs: add unstraightened emacs

--- system/substituters.nix
+++ system/substituters.nix
@@ -9,11 +9,13 @@
         "https://hyprland.cachix.org"
         "https://nyx.cachix.org"
         "https://soliprem.cachix.org"
+        "https://doom-emacs-unstraightened.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
         "soliprem.cachix.org-1:kwxQ2xmtoQJVrKUBvSkG/+03ZTuxn06Aebh8AZvtMPA="
+        "doom-emacs-unstraightened.cachix.org-1:O5oOlRPnmQEvVaFyuMTmthCEooHbrg54WgSLR07tmg4="
       ];
     };
     optimise.automatic = true;
