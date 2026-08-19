bc3ae2d9277f6ba1be63470252dd11a6dd55813f
Soliprem <franci.solidoro@gmail.com>
2026-08-19 12:49:41 +0200

    doom/init: add a few tools (pdf, spell / grammar checking, editorconfig, direnv)

--- doom/init.el
+++ doom/init.el
@@ -29,6 +29,8 @@
 
        :checkers
        syntax
+       (spell +flyspell) ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
 
        :tools
        (eval +overlay)
@@ -36,6 +38,21 @@
        lookup
        magit
        tree-sitter
+       ;;ansible
+       biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       direnv
+       ;;docker
+       editorconfig      ; let someone else argue about tabs vs spaces
+       ;;ein               ; tame Jupyter notebooks with emacs
+       ;;llm               ; when I said you needed friends, I didn't mean...
+       ;;make              ; run make tasks from Emacs
+       ;;pass              ; password manager for nerds
+       pdf               ; pdf enhancements
+       ;;terraform         ; infrastructure as code
+       ;;tmux              ; an API for interacting with tmux
+       ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
        (:if (featurep :system 'macos) macos)
