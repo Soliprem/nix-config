ea078145f06e023acb656e4486b9a0d9ffacbb3e
Soliprem <franci.solidoro@gmail.com>
2026-09-21 04:59:27 +0200

    doom/config: reorder file, add binds for supersonic

--- doom/config.el
+++ doom/config.el
@@ -1,54 +1,21 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
+;;; Appearance
+
 (setq doom-theme 'kusanagi
+      doom-font (font-spec :size 12.0)
       display-line-numbers-type 'relative
-      display-line-numbers-current-absolute t
-      auth-sources '("~/.authinfo" "/run/agenix/navidrome_authinfo")
-      org-directory "~/Documents/org/"
-      org-roam-directory org-directory
-      org-roam-dailies-directory "dailies/"
-      doom-font (font-spec :size 12.0))
+      display-line-numbers-current-absolute t)
 
-(after! emms
-  (require 'emms-player-mpv)
-  (setq emms-player-list '(emms-player-mpv)))
+;;; Authentication
 
-(use-package! empv
-  :bind-keymap ("C-c m" . empv-map)
-  :commands (empv-play empv-subsonic-search empv-subsonic-artists
-             empv-subsonic-albums empv-subsonic-songs)
-  :config
-  (require 'auth-source)
-  (keymap-set empv-map "/" #'empv-subsonic-search)
-  (keymap-set empv-map "A" #'empv-subsonic-artists)
-  (keymap-set empv-map "B" #'empv-subsonic-albums)
-  (keymap-set empv-map "S" #'empv-subsonic-songs)
-  (setq empv-subsonic-url "https://nv.soliprem.eu")
-  (when-let* ((auth (car (auth-source-search :host empv-subsonic-url
-                                           :require '(:user :secret)))))
-    (setq empv-subsonic-username (plist-get auth :user)
-          empv-subsonic-password (auth-info-password auth))))
+(setq auth-sources '("~/.authinfo" "/run/agenix/navidrome_authinfo"))
 
-(use-package! supersonic
-  :commands supersonic
-  :config
-  (require 'auth-source)
-  (setq supersonic-host "https://nv.soliprem.eu"))
+;;; Development
 
 (after! code-review
   (require 'ghub-legacy)
   (setq code-review-auth-login-marker 'forge))
-
-(use-package! wakatime-mode
-  :ensure t
-  :config
-  (global-wakatime-mode 1))
-
-(use-package! org-typst-preview
-  :after org)
-
-;; To enable it in every Org buffer
-(add-hook 'org-mode-hook #'org-typst-preview-mode)
 
 (use-package! pr-review
   :commands (pr-review
@@ -57,6 +24,17 @@
              pr-review-search-open)
   :init
   (setq pr-review-ghub-auth-name 'forge))
+
+(use-package! wakatime-mode
+  :ensure t
+  :config
+  (global-wakatime-mode 1))
+
+;;; Org
+
+(setq org-directory "~/Documents/org/"
+      org-roam-directory org-directory
+      org-roam-dailies-directory "dailies/")
 
 (after! org
   (custom-set-faces!
@@ -74,6 +52,14 @@
         org-roam-ui-follow t
         org-roam-ui-update-on-save t
         org-roam-ui-open-on-start t))
+
+(use-package! org-typst-preview
+  :after org)
+
+;; To enable it in every Org buffer
+(add-hook 'org-mode-hook #'org-typst-preview-mode)
+
+;;; Typst
 
 (use-package! typst-ts-mode
   :mode "\\.typ\\'"
@@ -111,6 +97,8 @@
       (funcall open-browser browser hostname)))
   (advice-add 'typst-preview--connect-browser :around
               #'+typst-preview-in-right-window))
+
+;;; Mail
 
 (set-email-account! "unibo"
   '((user-full-name . "Francesco Prem Solidoro")
@@ -165,3 +153,74 @@
         message-send-mail-function #'message-send-mail-with-sendmail
         mu4e-read-option-use-builtin nil
         mu4e-completing-read-function #'completing-read))
+
+;;; Music
+
+(after! emms
+  (require 'emms-player-mpv)
+  (setq emms-player-list '(emms-player-mpv)))
+
+(use-package! empv
+  :bind-keymap ("C-c m" . empv-map)
+  :commands (empv-play empv-subsonic-search empv-subsonic-artists
+             empv-subsonic-albums empv-subsonic-songs)
+  :config
+  (require 'auth-source)
+  (keymap-set empv-map "/" #'empv-subsonic-search)
+  (keymap-set empv-map "A" #'empv-subsonic-artists)
+  (keymap-set empv-map "B" #'empv-subsonic-albums)
+  (keymap-set empv-map "S" #'empv-subsonic-songs)
+  (setq empv-subsonic-url "https://nv.soliprem.eu")
+  (when-let* ((auth (car (auth-source-search :host empv-subsonic-url
+                                           :require '(:user :secret)))))
+    (setq empv-subsonic-username (plist-get auth :user)
+          empv-subsonic-password (auth-info-password auth))))
+
+(use-package! supersonic
+  :bind ("C-c s" . supersonic)
+  :config
+  (require 'auth-source)
+  (setq supersonic-host "https://nv.soliprem.eu"))
+
+(after! (supersonic evil)
+  (map! :map (supersonic-artist-mode-map supersonic-album-mode-map
+              supersonic-album-type-mode-map supersonic-tracks-mode-map
+              supersonic-search-mode-map supersonic-queue-mode-map
+              supersonic-now-playing-mode-map supersonic-podcast-mode-map
+              supersonic-podcast-episodes-mode-map)
+        :n "q" #'quit-window
+        :n "?" #'supersonic
+        :n "s" #'supersonic-search
+        :n "t" #'supersonic-toggle-playing
+        :n "]" #'supersonic-skip-track
+        :n "[" #'supersonic-prev-track
+        :n ">" #'supersonic-seek-forward
+        :n "<" #'supersonic-seek-back
+        :n "Q" #'supersonic-show-queue
+        :n "N" #'supersonic-show-now-playing)
+  (map! :map supersonic-artist-mode-map
+        :n "RET" #'supersonic-open-album
+        :n "gr" #'supersonic-artists-revert
+        :map (supersonic-album-mode-map supersonic-album-type-mode-map)
+        :n "RET" #'supersonic-open-tracks
+        :n "a" #'supersonic-enqueue-album
+        :map supersonic-tracks-mode-map
+        :n "RET" #'supersonic-play-tracks
+        :n "a" #'supersonic-enqueue-tracks
+        :map supersonic-search-mode-map
+        :n "RET" #'supersonic-open-search-result
+        :map supersonic-queue-mode-map
+        :n "gr" #'supersonic-queue-refresh
+        :map supersonic-now-playing-mode-map
+        :n "gr" #'supersonic-now-playing-refresh
+        :n "RET" #'push-button
+        :n "TAB" #'forward-button
+        :n "<backtab>" #'backward-button
+        :map supersonic-podcast-mode-map
+        :n "RET" #'supersonic-open-podcast-episodes
+        :n "a" #'supersonic-add-podcast
+        :n "?" #'supersonic-podcast-help
+        :map supersonic-podcast-episodes-mode-map
+        :n "RET" #'supersonic-play-podcast
+        :n "d" #'supersonic-download-podcast-episode
+        :n "?" #'supersonic-podcast-episode-help))
